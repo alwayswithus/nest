a87a26386d834b740eae86501a3486fa041edc03
hur-gilhaeng <hgh8401@gmail.com>
2020-05-15 14:05:07 +0900

    05.15 / 허길행 / nest프로젝트 백엔드 밑작업 완료.

--- nest/src/main/java/com/douzone/nest/controller/HelloController.java
+++ nest/src/main/java/com/douzone/nest/controller/HelloController.java
@@ -7,9 +7,68 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-	@ResponseBody
-	@RequestMapping("/hello")
+	@RequestMapping({"","/"})
 	public String hello() {
-		return "hello world";
+		return "/index.jsp";
+	}
+	
+	@RequestMapping("/pwfind")
+	public String hello01() {
+		return "";
+	}
+	
+	@RequestMapping("/signup")
+	public String hello02() {
+		return "";
+	}
+
+	@RequestMapping("/dashboard")
+	public String hello03() {
+		return "";
+	}
+	
+	@RequestMapping("/profile")
+	public String hello04() {
+		return "";
+	}
+	
+	@RequestMapping("/profileset")
+	public String hello05() {
+		return "";
+	}
+	
+	@RequestMapping("/notification")
+	public String hello06() {
+		return "";
+	}
+	
+	@RequestMapping("/setting")
+	public String hello07() {
+		return "";
+	}
+	
+	@RequestMapping("/comment")
+	public String hello08() {
+		return "";
+	}
+	
+	@RequestMapping("/file")
+	public String hello09() {
+		return "";
+	}
+	
+	@RequestMapping("/projectset")
+	public String hello10() {
+		return "";
+	}
+	
+	@RequestMapping("/kanbanMain")
+	public String hello11() {
+		return "";
+	}
+	
+	@RequestMapping("/gantt")
+	public String hello12() {
+		return "";
 	}
 }
