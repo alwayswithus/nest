061b7a2217c0145dd4f63366e4f2e3bd1c31860d
hur-gilhaeng <hgh8401@gmail.com>
2020-05-18 15:24:12 +0900

    05.18 / hgh / pom 수정 + bootap...수정..

--- nest/src/main/java/com/douzone/nest/BootApplication.java
+++ nest/src/main/java/com/douzone/nest/BootApplication.java
@@ -3,8 +3,10 @@ package com.douzone.nest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @ComponentScan({"com.douzone.nest.controller, com.douzone.nest.service, com.douzone.nest.repository"})
