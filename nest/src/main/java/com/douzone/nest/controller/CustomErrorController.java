c0a5d3993615c5cba24f09f371cf7c9528582e16
hur-gilhaeng <hgh8401@gmail.com>
2020-05-20 18:42:25 +0900

    05.20 / hgh / 통합 및 정리
    컨트롤러 정리...
    	hello 컨트롤러 제거.
    	main 컨트롤러 생성.
    	user 컨트롤러 생성.
    	커스텀 Error 컨트롤러 편집중..
    
    bundle.js 최신화

--- nest/src/main/java/com/douzone/nest/controller/CustomErrorController.java
+++ nest/src/main/java/com/douzone/nest/controller/CustomErrorController.java
@@ -2,15 +2,16 @@ package com.douzone.nest.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
-	@RequestMapping("/error")
-	public String handleError() {
-		return "index";
-	}
+	// error 편집중
+//	@RequestMapping("/error")
+//	public String handleError() {
+//		return "index";
+//	}
 
 	@Override
 	public String getErrorPath() {
--- nest/src/main/java/com/douzone/nest/controller/UserController.java
+++ nest/src/main/java/com/douzone/nest/controller/UserController.java
@@ -1,8 +1,19 @@
 package com.douzone.nest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class UserController {
+	
+	//로그인...
+	@PostMapping("/login")
+	public String login() {
+		System.out.println("로그인 시도중...");
+		return "redirect:/dashboard";
+	}
+	
 	
 	// 비밀번호 찾기
 	@RequestMapping("/pwfind")
