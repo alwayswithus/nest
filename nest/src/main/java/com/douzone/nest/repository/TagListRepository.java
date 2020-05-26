88aab9e528f2182a68baa659d058b3da88920e24
Kim-woo-gyeong <lovelykwg@naver.com>
2020-05-26 17:00:52 +0900

    0526 / 김우경 / tagList insert delete 수정 중

---
+++ nest/src/main/java/com/douzone/nest/repository/TagListRepository.java
@@ -0,0 +1,29 @@
+package com.douzone.nest.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.douzone.nest.vo.TagListVo;
+
+@Repository
+public class TagListRepository {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	public List<TagListVo> selectTagList() {
+		return sqlSession.selectList("taglist.selectTagList");
+	}
+	public int tagInsert(TagListVo tagListVo) {
+		return sqlSession.insert("taglist.insertTagList", tagListVo);
+		
+	}
+	public int tagDelete(Long tagNo) {
+		int tagList = sqlSession.delete("taglist.deleteTagList", tagNo);
+		int tagTask = sqlSession.delete("taglist.deleteTagTask", tagNo);
+		return tagList  + tagTask ;
+	}
+
+}
