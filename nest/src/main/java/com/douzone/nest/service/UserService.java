72c08992f6258924670aeec03fa96d7cbfcca3ab
hur-gilhaeng <hgh8401@gmail.com>
2020-05-26 17:02:07 +0900

    05.26 / hgh / 로그인 작업중...

--- nest/src/main/java/com/douzone/nest/service/UserService.java
+++ nest/src/main/java/com/douzone/nest/service/UserService.java
@@ -1,5 +1,22 @@
 package com.douzone.nest.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.douzone.nest.repository.UserRepository;
+import com.douzone.nest.vo.UserVo;
+
+@Service
 public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	// DB에서 유저 가저오기
+	public UserVo getUser(UserVo vo) {
+		UserVo result = userRepository.findByEmailAndPassword(vo);
+		System.out.println(result);
+		return result;
+	}
 
 }
