7249883f20a6a27e1928ab33117229b44e4a5196
inhyo <inhyo825@gmail.com>
2020-05-26 17:03:08 +0900

    0526 / 최인효 / 칸반보드 select react랑 연결

--- nest/src/main/java/com/douzone/nest/vo/TagListVo.java
+++ nest/src/main/java/com/douzone/nest/vo/TagListVo.java
@@ -1,24 +1,47 @@
 package com.douzone.nest.vo;
 
 public class TagListVo {
-	private Long tagNo; //태그번호
-	private String tagName; //태그이름
-	
+	private Long tagNo; // 태그번호
+	private String tagName; // 태그이름
+	private String tagColor; // 태그이름
+	private Long taskNo; // 업무번호
+
 	public Long getTagNo() {
 		return tagNo;
 	}
+
 	public void setTagNo(Long tagNo) {
 		this.tagNo = tagNo;
 	}
+
 	public String getTagName() {
 		return tagName;
 	}
+
 	public void setTagName(String tagName) {
 		this.tagName = tagName;
 	}
-	
+
+	public String getTagColor() {
+		return tagColor;
+	}
+
+	public void setTagColor(String tagColor) {
+		this.tagColor = tagColor;
+	}
+
+	public Long getTaskNo() {
+		return taskNo;
+	}
+
+	public void setTaskNo(Long taskNo) {
+		this.taskNo = taskNo;
+	}
+
 	@Override
 	public String toString() {
-		return "TagListVo [tagNo=" + tagNo + ", tagName=" + tagName + "]";
+		return "TagListVo [tagNo=" + tagNo + ", tagName=" + tagName + ", tagColor=" + tagColor + ", taskNo=" + taskNo
+				+ "]";
 	}
+
 }
