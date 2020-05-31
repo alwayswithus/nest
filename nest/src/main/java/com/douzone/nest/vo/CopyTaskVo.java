443bb98a7a90cc61c728de07769908507e37d738
inhyo <inhyo825@gmail.com>
2020-05-31 16:31:04 +0900

    0531 / 최인효 / 칸반보드 데이터 처리 끝

---
+++ nest/src/main/java/com/douzone/nest/vo/CopyTaskVo.java
@@ -0,0 +1,58 @@
+package com.douzone.nest.vo;
+
+import java.util.List;
+
+public class CopyTaskVo {
+	private Long originalTaskNo; 
+	private Long taskNo;
+	private List<TagListVo> tagList; 
+	private List<UserVo> memberList; 
+	private List<CheckListVo> checkList; 
+
+	public Long getOriginalTaskNo() {
+		return originalTaskNo;
+	}
+
+	public void setOriginalTaskNo(Long originalTaskNo) {
+		this.originalTaskNo = originalTaskNo;
+	}
+
+	public Long getTaskNo() {
+		return taskNo;
+	}
+
+	public void setTaskNo(Long taskNo) {
+		this.taskNo = taskNo;
+	}
+
+	public List<TagListVo> getTagList() {
+		return tagList;
+	}
+
+	public void setTagList(List<TagListVo> tagList) {
+		this.tagList = tagList;
+	}
+
+	public List<UserVo> getMemberList() {
+		return memberList;
+	}
+
+	public void setMemberList(List<UserVo> memberList) {
+		this.memberList = memberList;
+	}
+
+	public List<CheckListVo> getCheckList() {
+		return checkList;
+	}
+
+	public void setCheckList(List<CheckListVo> checkList) {
+		this.checkList = checkList;
+	}
+
+	@Override
+	public String toString() {
+		return "CopyTaskVo [originalTaskNo=" + originalTaskNo + ", taskNo=" + taskNo + ", tagList=" + tagList
+				+ ", memberList=" + memberList + ", checkList=" + checkList + "]";
+	}
+
+}
