4b627a88db9f18eb83025fb76014d701d3eedccf
Kim-woo-gyeong <lovelykwg@naver.com>
2020-06-18 20:37:00 +0900

    0618 / 김우경 / history

--- nest/src/main/java/com/douzone/nest/service/HistoryService.java
+++ nest/src/main/java/com/douzone/nest/service/HistoryService.java
@@ -31,9 +31,44 @@ public class HistoryService {
 		map.put("logDate", historyJson.get("historyDate"));
 		map.put("projectNo", historyJson.get("projectNo"));
 		
+		System.out.println(historyJson);
+		
 		switch((String) historyJson.get("historyType")) {
 			case "taskContentsUpdate":
 				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + "으로 업무이름을 수정하셨습니다.");
+				break;
+			case "taskListInsert":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무리스트를 추가하였습니다.");
+				break;
+			case "taskListDelete":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무리스트를 삭제하였습니다.");
+				break;
+			case "taskDateUpdate":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무의 마감일을 수정하였습니다.");
+				break;
+			case "taskMemberJoin":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무에 멤버를 추가하였습니다.");
+				break;
+			case "checklistInsert":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무에 체크리스트를 추가하였습니다.");
+				break;
+			case "checklistStateUpdate":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무의 체크리스트 상태를 수정하였습니다.");
+				break;
+			case "commentInsert":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무에 코멘트를 추가하였습니다.");
+				break;
+			case "taskDragNdrop":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무의 위치를 변경하였습니다.");
+				break;
+			case "taskStateUpdate":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무 상태를 변경하였습니다.");
+				break;
+			case "taskInsert":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무를 추가하였습니다.");
+				break;
+			case "taskDelete":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무를 삭제하였습니다.");
 				break;
 		}
 		
